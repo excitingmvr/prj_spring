0dcc9403b6da9c47ec5138681143fa28416dc094
excitingmvr <excitingmvr@gmail.com>
2023-06-21 17:22:35 +0900

    first

---
+++ src/main/java/com/mycompany/app/home/HomeController.java
@@ -0,0 +1,32 @@
+package com.mycompany.app.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/" )
+	public String home() {
+		// 여기에서 서버단에서 필요한 작업을 수행한다.
+		System.out.println("asdfasf");
+		// href="/resources/xdm/v1/css/commonXdm.css"
+		
+		// 아래의 jsp 파일 호출
+		return "home";
+	}
+	
+	
+	@RequestMapping(value = "/home2" )
+	public String home2() {
+		// 여기에서 서버단에서 필요한 작업을 수행한다.
+		
+		
+		// 아래의 jsp 파일 호출
+		return "app/home/home2";
+	}
+
+	
+
+
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8" %>
+home.jsp
+asdfasdf
+장원영
+
+
+ㅁㄴㅇㄻㄴㅇㄹ
+
+
+ㅁㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻ
