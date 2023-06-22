874b0cceee274c6192659d95cc13873050aab3c2
excitingmvr <excitingmvr@gmail.com>
2023-06-22 17:29:13 +0900

    sadfg

--- src/main/java/com/mycompany/app/home/HomeController.java
+++ src/main/java/com/mycompany/app/home/HomeController.java
@@ -2,6 +2,7 @@ package com.mycompany.app.home;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -25,7 +26,53 @@ public class HomeController {
 		// 아래의 jsp 파일 호출
 		return "app/home/home2";
 	}
+	
+	@RequestMapping(value = "/home4" )
+	public String home4() {
+		// 여기에서 서버단에서 필요한 작업을 수행한다.		
+		
+		
+		// 아래의 jsp 파일 호출         
+		return "app/home/home2";
+	}
+	
+	@RequestMapping(value = "/home5" )
+	public String home5() {
+		// 여기에서 서버단에서 필요한 작업을 수행한다.
+		
+		
+		// 아래의 jsp 파일 호출
+		return "app/home/home2";
+	}
 
+	
+	@RequestMapping(value = "/home3")
+	public ModelAndView home3() {
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("home3");
+		
+		return mav;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 
 
