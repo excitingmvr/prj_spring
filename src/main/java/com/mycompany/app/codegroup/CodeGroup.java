b6d35b42d4178d93ffeb7814c99e755c8b050ccf
excitingmvr <excitingmvr@gmail.com>
2023-06-23 17:13:23 +0900

    f.codegroup

---
+++ src/main/java/com/mycompany/app/codegroup/CodeGroup.java
@@ -0,0 +1,123 @@
+package com.mycompany.app.codegroup;
+
+public class CodeGroup {
+
+	// 디비컬럼명 첫글자는 소문자
+	// seq는 편의상 디비에는 int 지만 자바에서는 String 으로 사용한다.
+	// 주로 테이블의 컬럼명과 일치
+	
+	private String ifcgSeq;
+	private String ifcgSeqAnother;
+	private String ifcgName;
+	private String ifcgNameEng;
+	private Integer ifcgUseNy;
+	private Integer ifcgOrder;
+	private String ifcgReferenceV1;
+	private String ifcgReferenceV2;
+	private String ifcgReferenceV3;
+	private String ifcgReferenceV4;
+	private Integer ifcgReferenceI1;
+	private Integer ifcgReferenceI2;
+	private Integer ifcgReferenceI3;
+	private Integer ifcgReferenceI4;
+	private String ifcgDesc;
+	private Integer ifcgDelNy;
+//	-----
+	public String getIfcgSeq() {
+		return ifcgSeq;
+	}
+	public void setIfcgSeq(String ifcgSeq) {
+		this.ifcgSeq = ifcgSeq;
+	}
+	public String getIfcgSeqAnother() {
+		return ifcgSeqAnother;
+	}
+	public void setIfcgSeqAnother(String ifcgSeqAnother) {
+		this.ifcgSeqAnother = ifcgSeqAnother;
+	}
+	public String getIfcgName() {
+		return ifcgName;
+	}
+	public void setIfcgName(String ifcgName) {
+		this.ifcgName = ifcgName;
+	}
+	public String getIfcgNameEng() {
+		return ifcgNameEng;
+	}
+	public void setIfcgNameEng(String ifcgNameEng) {
+		this.ifcgNameEng = ifcgNameEng;
+	}
+	public Integer getIfcgUseNy() {
+		return ifcgUseNy;
+	}
+	public void setIfcgUseNy(Integer ifcgUseNy) {
+		this.ifcgUseNy = ifcgUseNy;
+	}
+	public Integer getIfcgOrder() {
+		return ifcgOrder;
+	}
+	public void setIfcgOrder(Integer ifcgOrder) {
+		this.ifcgOrder = ifcgOrder;
+	}
+	public String getIfcgReferenceV1() {
+		return ifcgReferenceV1;
+	}
+	public void setIfcgReferenceV1(String ifcgReferenceV1) {
+		this.ifcgReferenceV1 = ifcgReferenceV1;
+	}
+	public String getIfcgReferenceV2() {
+		return ifcgReferenceV2;
+	}
+	public void setIfcgReferenceV2(String ifcgReferenceV2) {
+		this.ifcgReferenceV2 = ifcgReferenceV2;
+	}
+	public String getIfcgReferenceV3() {
+		return ifcgReferenceV3;
+	}
+	public void setIfcgReferenceV3(String ifcgReferenceV3) {
+		this.ifcgReferenceV3 = ifcgReferenceV3;
+	}
+	public String getIfcgReferenceV4() {
+		return ifcgReferenceV4;
+	}
+	public void setIfcgReferenceV4(String ifcgReferenceV4) {
+		this.ifcgReferenceV4 = ifcgReferenceV4;
+	}
+	public Integer getIfcgReferenceI1() {
+		return ifcgReferenceI1;
+	}
+	public void setIfcgReferenceI1(Integer ifcgReferenceI1) {
+		this.ifcgReferenceI1 = ifcgReferenceI1;
+	}
+	public Integer getIfcgReferenceI2() {
+		return ifcgReferenceI2;
+	}
+	public void setIfcgReferenceI2(Integer ifcgReferenceI2) {
+		this.ifcgReferenceI2 = ifcgReferenceI2;
+	}
+	public Integer getIfcgReferenceI3() {
+		return ifcgReferenceI3;
+	}
+	public void setIfcgReferenceI3(Integer ifcgReferenceI3) {
+		this.ifcgReferenceI3 = ifcgReferenceI3;
+	}
+	public Integer getIfcgReferenceI4() {
+		return ifcgReferenceI4;
+	}
+	public void setIfcgReferenceI4(Integer ifcgReferenceI4) {
+		this.ifcgReferenceI4 = ifcgReferenceI4;
+	}
+	public String getIfcgDesc() {
+		return ifcgDesc;
+	}
+	public void setIfcgDesc(String ifcgDesc) {
+		this.ifcgDesc = ifcgDesc;
+	}
+	public Integer getIfcgDelNy() {
+		return ifcgDelNy;
+	}
+	public void setIfcgDelNy(Integer ifcgDelNy) {
+		this.ifcgDelNy = ifcgDelNy;
+	}
+	
+}
