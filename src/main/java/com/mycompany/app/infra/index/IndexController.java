0d99321d4018c4d435675e0c2aa3a9a47d5925e4
excitingmvr <excitingmvr@gmail.com>
2023-06-28 12:02:09 +0900

    asdf

---
+++ src/main/java/com/mycompany/app/infra/index/IndexController.java
@@ -0,0 +1,19 @@
+package com.mycompany.app.infra.index;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/biographyUsrView")
+	public String biographyUsrView() {
+		return "usr/infra/biography/biographyUsrView";
+	}
+	
+	
+	@RequestMapping("/indexUsrView")
+	public String indexUsrView() {
+		return "usr/infra/index/indexUsrView";
+	}
+}
